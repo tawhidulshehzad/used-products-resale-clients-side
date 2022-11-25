c587b70e7780eb8b696a402d4a0c3a5cf98ad750
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-25 12:25:24 +0600

    set up basic

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCAu_Q9iICmX91ar1wc6PTiuN9V7_yb9gY",
+  authDomain: "bookworm-b5f6e.firebaseapp.com",
+  projectId: "bookworm-b5f6e",
+  storageBucket: "bookworm-b5f6e.appspot.com",
+  messagingSenderId: "456576429743",
+  appId: "1:456576429743:web:26a458f1e3a16a12c82e5a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
