706559cc60ecd915391311ef813d0996eb57c104
shehzad1212 <tawhidulislam261@gmail.com>
2022-11-25 15:12:25 +0600

    nav item added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./firebase/Routes/Routes/Routes";
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="max-w-[1440px] mx-auto">
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from "react";
+import oldbook from "../../../assets/banner.avif";
+
+const Banner = () => {
+  return (
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div>
+          <h1 className="text-5xl font-bold">So many books, so little time</h1>
+          <p className="py-6">
+            The person, be it gentleman or lady, who has not pleasure in a good
+            novel, must be intolerably stupid
+          </p>
+          <button className="btn">Get started</button>
+        </div>
+        <img src={oldbook} className="rounded-lg lg:w-1/2 shadow-2xl" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
